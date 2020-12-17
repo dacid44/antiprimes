da1630d9df41d38d361862b232aa66c2c79664fb
Keegan Conlee <53133576+dacid44@users.noreply.github.com>
2020-12-16 23:37:07 -0600

    cleaned up file and optimized batch sizes

--- antiprimes_4_gpu.py
+++ antiprimes_4_gpu.py
@@ -3,26 +3,31 @@ from pycuda.compiler import SourceModule
 import pycuda as cuda
 import pycuda.autoinit
 import numpy as np
-from numba import jit, njit, prange
+from numba import njit, prange
 from time import time
 
-BATCH_SIZE = 196608 # // 2
-GRID_SIZE = 256
+BATCH_MULTIPLIER = 64
+BATCH_SIZE = int(196608 * BATCH_MULTIPLIER)
+GRID_SIZE = int(256 // BATCH_MULTIPLIER)
+GEN_TESTS = False
 
-limit = 100000000 # limit = int(input('Limit: '))
+limit = 1000000000  # limit = int(input('Limit: '))
 
+# noinspection PyStringFormat
 mod_gen = SourceModule("""
-__global__ void mod_gen(unsigned int *x, unsigned {0} *y, unsigned int *stop, int width) {{
+__global__ void mod_gen({1}unsigned int *y, unsigned {0} *z, unsigned int *stop, int width) {{
   const int n = blockIdx.x * 1024 + threadIdx.x;
   const int s = blockIdx.y * width + n;
   const unsigned short size = gridDim.y;
   if (n < width) {{
     for (int i = blockIdx.y + 1; i < stop[n] - 1; i += size) {{
-      y[s] += (x[n] % i) == 0;
+      z[s] += (y[n] % {2}) == 0;
     }}
   }}
 }}
-""".format('short' if limit <= 1000000000 else 'int')).get_function("mod_gen")
+""".format('short' if limit <= 1000000000 else 'int',
+           *(('', 'i') if GEN_TESTS else ('unsigned int *x, ', 'x[i]'))
+           )).get_function("mod_gen")
 n_data_type = np.uint16 if limit <= 1000000000 else np.uint32
 
 start_time = time()
@@ -41,11 +46,19 @@ def count_factors(results):
 def test_factor_batch(batch, mem):
     sqrts = np.sqrt(batch)
     ceils = np.ceil(sqrts).astype(dtype=np.uint32)
+    if GEN_TESTS:
+        mod_args = (gpuarray.to_gpu(batch, mem.allocate),)
+    else:
+        test_mat = np.arange(1, ceils[-1], dtype=np.uint32)
+        mat_gpu = gpuarray.to_gpu(test_mat, allocator=mem.allocate)
+        mod_args = (mat_gpu, gpuarray.to_gpu(batch, mem.allocate))
     res_mat = np.zeros((min(GRID_SIZE, ceils[-1] - 1), batch.size), dtype=n_data_type)
     res_gpu = gpuarray.to_gpu(res_mat, allocator=mem.allocate)
-    mod_gen(gpuarray.to_gpu(batch, mem.allocate), res_gpu, gpuarray.to_gpu(ceils, allocator=mem.allocate),
+    mod_gen(*mod_args, res_gpu, gpuarray.to_gpu(ceils, allocator=mem.allocate),
             np.uint32(batch.size), block=(1024, 1, 1), grid=(BATCH_SIZE // 1024 + 1, GRID_SIZE))
     res_mat = res_gpu.get()
+    if not GEN_TESTS:
+        del mat_gpu
     del res_gpu
     num_factors = count_factors(res_mat)
     num_factors += np.equal(sqrts, ceils)
