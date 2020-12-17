6c632df0364543648836e13698e9e6cd15162bc7
Keegan Conlee <53133576+dacid44@users.noreply.github.com>
2020-12-17 02:00:55 -0600

    added 64-bit support for large numbers

--- antiprimes_4_gpu.py
+++ antiprimes_4_gpu.py
@@ -15,29 +15,30 @@ limit = 100000000  # limit = int(input('Limit: '))
 
 # noinspection PyStringFormat
 mod_gen = SourceModule("""
-__global__ void mod_gen({1}unsigned int *y, unsigned {0} *z, unsigned int *stop, int width) {{
+__global__ void mod_gen({1}unsigned {{0}} *y, unsigned {0} *z, unsigned {{0}} *stop, int width) {{{{
   const int n = blockIdx.x * 1024 + threadIdx.x;
   const int s = blockIdx.y * width + n;
   const unsigned short size = gridDim.y;
-  if (n < width) {{
-    for (int i = blockIdx.y{2}; i < stop[n]{3}; i += size) {{
+  if (n < width) {{{{
+    for (int i = blockIdx.y{2}; i < stop[n]{3}; i += size) {{{{
       z[s] += (y[n] % {4}) == 0;
-    }}
-  }}
-}}
+    }}}}
+  }}}}
+}}}}
 """.format('short' if limit <= 1000000000 else 'int',
-           *(('', ' + 1', '', 'i') if GEN_TESTS else ('unsigned int *x, ', '', ' - 1', 'x[i]'))
-           )).get_function("mod_gen")
+           *(('', ' + 1', '', 'i') if GEN_TESTS else ('unsigned {0} *x, ', '', ' - 1', 'x[i]'))
+           ).format('int' if limit <= 1000000000 else 'long long')).get_function("mod_gen")
 n_data_type = np.uint16 if limit <= 1000000000 else np.uint32
+m_data_type = np.uint32 if limit <= 1000000000 else np.uint64
 
 start_time = time()
 
-most = np.uint32(0)
+most = m_data_type(0)
 antiprimes = []
 
 @njit(parallel=True)
 def count_factors(results):
-    to_return = np.zeros(results.shape[1], dtype=np.uint32)
+    to_return = np.zeros(results.shape[1], dtype=m_data_type)
     for n in prange(results.shape[1]):
         to_return[n] = np.sum(results[:, n])
         to_return[n] *= 2
@@ -45,17 +46,17 @@ def count_factors(results):
 
 def test_factor_batch(batch, mem):
     sqrts = np.sqrt(batch)
-    ceils = np.ceil(sqrts).astype(dtype=np.uint32)
+    ceils = np.ceil(sqrts).astype(dtype=m_data_type)
     if GEN_TESTS:
         mod_args = (gpuarray.to_gpu(batch, mem.allocate),)
     else:
-        test_mat = np.arange(1, ceils[-1], dtype=np.uint32)
+        test_mat = np.arange(1, ceils[-1], dtype=m_data_type)
         mat_gpu = gpuarray.to_gpu(test_mat, allocator=mem.allocate)
         mod_args = (mat_gpu, gpuarray.to_gpu(batch, mem.allocate))
     res_mat = np.zeros((min(GRID_SIZE, ceils[-1] - 1), batch.size), dtype=n_data_type)
     res_gpu = gpuarray.to_gpu(res_mat, allocator=mem.allocate)
     mod_gen(*mod_args, res_gpu, gpuarray.to_gpu(ceils, allocator=mem.allocate),
-            np.uint32(batch.size), block=(1024, 1, 1), grid=(BATCH_SIZE // 1024 + 1, GRID_SIZE))
+            m_data_type(batch.size), block=(1024, 1, 1), grid=(BATCH_SIZE // 1024 + 1, GRID_SIZE))
     res_mat = res_gpu.get()
     if not GEN_TESTS:
         del mat_gpu
@@ -91,7 +92,7 @@ def file_print(print_obj, out_file='out.txt'):
 gpu_mem = cuda.tools.DeviceMemoryPool()
 for n in range(1, limit + 1, BATCH_SIZE):
     n_max = limit + 1 if limit < n + BATCH_SIZE else n + BATCH_SIZE
-    run_factor_batch(np.arange(n, n_max, dtype=np.uint32), gpu_mem)
+    run_factor_batch(np.arange(n, n_max, dtype=m_data_type), gpu_mem)
 
 file_print(antiprimes)
 file_print(time() - start_time)
